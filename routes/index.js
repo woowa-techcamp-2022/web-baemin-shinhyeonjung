db6e550f74156dec1064e116cfe4b0559708d20d
HyeonDoonge <migu554@naver.com>
2022-07-07 22:43:05 +0900

    feat: 메인 페이지 로그인된 사용자 정보 렌더링

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+const { findSessionBySessionId, findUserById } = require('../db/utils');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  // 이때 사용자의 정보 확인!
+  const { cookies } = req;
+
+  console.log(cookies);
+  if (cookies.session_id) {
+    const { sessionId, userId } = findSessionBySessionId(cookies.session_id);
+    const user = findUserById(userId);
+    res.render('index', { nickname: user.nickname, profileFileName: user.profileFileName });
+  } else {
+    res.render('index');
+  }
 });
 
 module.exports = router;
